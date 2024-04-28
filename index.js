79c1edc2225c85d1648598a67dd1e9747c2b02ae
Laura Mañogil <laura.manogil@sembo.se>
2024-04-28 20:21:55 +0200

    add customized CORS headers

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(router);
 server.use(middlewares);
 
